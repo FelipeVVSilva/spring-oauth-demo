fb9522c95331a24530dbec7bcc46704fe3284cf4
Felipe Veiga <felipe.veiga.trabalho@gmail.com>
2024-09-17 15:34:04 -0300

    Role class created

--- src/main/java/com/demo/oauth/entities/User.java
+++ src/main/java/com/demo/oauth/entities/User.java
@@ -1,13 +1,18 @@
 package com.demo.oauth.entities;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +28,13 @@ public class User {
     private String email;
     private String password;
 
+    @ManyToMany
+    @JoinTable(
+    		name = "tb_user_role",
+    		joinColumns = @JoinColumn(name = "user_id"),
+    		inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+     
     public User() {
     }
 
@@ -36,34 +48,35 @@ public class User {
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
+    public boolean hasRole(String roleName) {
+		for(Role role : roles) {
+			if(role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
     @Override
     public boolean equals(Object o) {
@@ -74,7 +87,6 @@ public class User {
 
         return Objects.equals(id, user.id);
     }
-
     @Override
     public int hashCode() {
         return id != null ? id.hashCode() : 0;
