1571b2a974e0bf92f90813f2f800062885c3b4cd
Felipe Veiga <felipe.veiga.trabalho@gmail.com>
2024-09-17 16:31:08 -0300

    Adding PasswordEncoder

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$AXPPAlByqQtjjGZjNAOlveIGIrFm5oD8IybaW1h72eAgKrrS9MbF.');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$sam.n6s0mWgct.X4Fj9VC.2JfXQYwvwd/CSMG/k38IkiJ5xqDmaWe');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
